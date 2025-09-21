75e643be514e5e9425b0c50b35201dbe2b49deda
Ashen Deelaka <122264429+ashendeelaka@users.noreply.github.com>
2025-09-21 20:47:30 +0530

    initial commit from vite react

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='text-3xl font-bold underline text-amber-200'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
